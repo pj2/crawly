f9170d6f1f4e2afc28452d282258d905abe8ba72
Joshua Prendergast <joshua.prendergast@wandera.com>
2016-10-26 18:49:12 +0100

    main: add parameter validation

--- crawly/__main__.py
+++ crawly/__main__.py
@@ -1,3 +1,4 @@
+import sys
 import argparse
 import logging
 from .crawler import Crawler
@@ -6,16 +7,21 @@ from .url import URL
 
 if __name__ == '__main__':
     argp = argparse.ArgumentParser('crawly')
-    argp.add_argument('url', help='target URL to crawl')
+    argp.add_argument('url', help='target URL to crawl e.g. http://www.google.com')
     argp.add_argument('-v', action='store_true', help='produce more output')
     argp.add_argument('-s', action='store_true', help='print out static files listing')
 
     args = argp.parse_args()
+    url = URL(args.url)
+
+    if url.is_relative() or not url.parts.scheme:
+        print 'invalid url'
+        sys.exit(1)
+
     logging.basicConfig()
     logging.getLogger().setLevel(logging.DEBUG if args.v else logging.ERROR)
 
     try:
-        # TODO Check this is a valid URL
-        Crawler(URL(args.url), args.s).crawl()
+        Crawler(url, args.s).crawl()
     except (KeyboardInterrupt, EOFError):
         pass
